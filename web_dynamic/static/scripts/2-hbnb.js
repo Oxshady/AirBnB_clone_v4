5b902799a42df980a7eafaf043575f6f74999ce1
Oxshady <shadymahmod55@gmail.com>
2024-08-12 02:21:39 +0300

    done ^_^

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,27 @@
-$(document).ready(function() {
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-        if (data.status === "OK") {
-            $('#api_status').addClass('available');
-        } else {
-            $('#api_status').removeClass('available');
-        }
-    });
+$(document).ready(function () {
+	let aminities = {};
+	$('.inp_checkbox').change(function () {
+		const Aid = $(this).attr('data-id');
+		const Aname = $(this).attr('data-name');
+		if ($(this).is(':checked')) {
+			aminities[Aid] = Aname;
+		} else {
+			delete aminities[Aid];
+		}
+		let amenitiesList = Object.values(aminities);
+		if (amenitiesList.length > 3) {
+			amenitiesList = amenitiesList.slice(0, 3);
+			amenitiesList.push('...');
+		}
+		$('.amenities h4').text(amenitiesList.join(', '));
+	});
+	$.get('http://127.0.0.1:5050/api/v1/status/', function (data) {
+		if (data.status === "OK") {
+			console.log(data)
+			console.log("Sadsdnaklds")
+			$('.api_status').addClass('available');
+		} else {
+			$('.api_status').removeClass('available');
+		}
+	});
 });
