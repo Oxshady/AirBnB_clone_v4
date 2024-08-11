6174bb4c530b541cfb13de814a7ba0594e89f332
Oxshady <shadymahmod55@gmail.com>
2024-08-12 02:27:59 +0300

    remove console log that i make for debugging xD

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,10 +15,8 @@ $(document).ready(function () {
 		}
 		$('.amenities h4').text(amenitiesList.join(', '));
 	});
-	$.get('http://127.0.0.1:5050/api/v1/status/', function (data) {
+	$.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
 		if (data.status === "OK") {
-			console.log(data)
-			console.log("Sadsdnaklds")
 			$('.api_status').addClass('available');
 		} else {
 			$('.api_status').removeClass('available');
